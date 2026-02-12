9a8e619a0fe3dac9bc1dc499f35df2de40547701
modernmania <amodernanimator@gmail.com>
2026-02-11 19:55:33 -0500

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+# docs/conf.py
+project = 'Emerald'
+copyright = '2026, Gem Creative'
+author = 'Gem Creative'
+
+extensions = [
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
