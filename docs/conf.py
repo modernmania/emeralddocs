b06d2b06d28017c4efdf795c2206993b4ff88bd8
modernmania <amodernanimator@gmail.com>
2026-02-11 20:01:59 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,23 @@ extensions = [
     'myst_parser',
 ]
 
+html_theme = 'furo'
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#2ecc71",
+        "color-brand-content": "#27ae60",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#2ecc71",
+        "color-brand-content": "#2ecc71",
+        "color-sidebar-background": "#121212",
+    },
+}
+
+html_logo = "_static/emer.png"
+html_favicon = "_static/emer.png"
+html_static_path = ['_static']
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'
